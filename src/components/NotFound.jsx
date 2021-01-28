f1e83b8dbe5bd196b516a55bafe08932859fe666
mcidoni <matthewcidoni@gmail.com>
2021-01-28 15:05:09 -0500

    init commit

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>Looking for something?</h1>
+      <p>
+        <h1>error</h1>
+      </p>
+    </>
+  )
+}
+
+export default NotFound
